a71baec900bfefb21c3ba6070f41206b478fc2f7
chourmovs <chourmovs@gmail.com>
2025-04-26 13:06:13 +0200

    fix 6

--- custom_components/urbansolar_battery/config_flow.py
+++ custom_components/urbansolar_battery/config_flow.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.core import callback
-from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers import entity_registry as er
 
 from .const import DOMAIN, CONF_SOURCE_SENSOR
@@ -25,7 +24,7 @@ class UrbanSolarBatteryFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
                 errors["base"] = "invalid_entity"
             else:
                 await self.async_set_unique_id(DOMAIN)
-                self._abort_if_unique_id_configured()  # Correction ici
+                self._abort_if_unique_id_configured()
                 return self.async_create_entry(
                     title="Configuration Batterie Virtuelle",
                     data=user_input,
@@ -37,11 +36,10 @@ class UrbanSolarBatteryFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
                 vol.Required(
                     CONF_SOURCE_SENSOR,
                     default="sensor.default_energy_sensor"
-                ): str  # <--- ici, correction
+                ): str  # UTILISER str et pas cv.entity_id
             }),
             errors=errors,
-)
-
+        )
 
     @staticmethod
     @callback
@@ -57,19 +55,27 @@ class UrbanSolarBatteryOptionsFlowHandler(config_entries.OptionsFlow):
 
     async def async_step_init(self, user_input=None):
         """Manage the options."""
+        errors = {}
+
         if user_input is not None:
-            self.hass.config_entries.async_update_entry(
-                self.config_entry,
-                data={**self.config_entry.data, **user_input}
-            )
-            return self.async_create_entry(title="", data=user_input)
+            entity_id = user_input[CONF_SOURCE_SENSOR]
+            entity_registry = er.async_get(hass=self.hass)
+            if not entity_registry.async_get(entity_id):
+                errors["base"] = "invalid_entity"
+            else:
+                self.hass.config_entries.async_update_entry(
+                    self.config_entry,
+                    data={**self.config_entry.data, **user_input}
+                )
+                return self.async_create_entry(title="", data=user_input)
 
         return self.async_show_form(
             step_id="init",
             data_schema=vol.Schema({
                 vol.Optional(
                     CONF_SOURCE_SENSOR,
-                    default=self.config_entry.data.get(CONF_SOURCE_SENSOR)
-                ): cv.entity_id
-            })
+                    default=self.config_entry.data.get(CONF_SOURCE_SENSOR, "sensor.default_energy_sensor")
+                ): str  # ici aussi str et PAS cv.entity_id
+            }),
+            errors=errors,
         )
