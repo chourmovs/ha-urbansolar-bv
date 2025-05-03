dcfabe15f86e6fb886a4a12b0e022e669cf54f35
chourmovs <chourmovs@gmail.com>
2025-05-03 07:47:39 +0200

    better config flow

--- custom_components/urbansolar_battery/config_flow.py
+++ custom_components/urbansolar_battery/config_flow.py
@@ -1,9 +1,13 @@
 from homeassistant import config_entries
-import voluptuous as vol
 from homeassistant.core import callback
+import voluptuous as vol
 
+from homeassistant.helpers.selector import (
+    selector,
+)
 
 from .const import DOMAIN, CONF_PRODUCTION_SENSOR, CONF_CONSOMMATION_SENSOR
+
 
 class VirtualBatteryConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Config flow for UrbanSolar Virtual Battery."""
@@ -13,31 +17,55 @@ class VirtualBatteryConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         if user_input is not None:
             return self.async_create_entry(title="UrbanSolar Battery", data=user_input)
 
-        schema = vol.Schema({
-            vol.Required(CONF_PRODUCTION_SENSOR): str,
-            vol.Required(CONF_CONSOMMATION_SENSOR): str,
-        })
-
-        return self.async_show_form(step_id="user", data_schema=schema, errors=errors)
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({
+                vol.Required(CONF_PRODUCTION_SENSOR): selector({
+                    "entity": {
+                        "domain": "sensor",
+                        "device_class": "energy",  # uniquement les capteurs avec device_class "energy"
+                    }
+                }),
+                vol.Required(CONF_CONSOMMATION_SENSOR): selector({
+                    "entity": {
+                        "domain": "sensor",
+                        "device_class": "energy",
+                    }
+                }),
+            }),
+            errors=errors,
+        )
 
     @staticmethod
     @callback
     def async_get_options_flow(config_entry):
         return VirtualBatteryOptionsFlowHandler(config_entry)
 
+
 class VirtualBatteryOptionsFlowHandler(config_entries.OptionsFlow):
     """Options flow for UrbanSolar Virtual Battery."""
 
     def __init__(self, config_entry):
-        self._config_entry = config_entry
+        self.config_entry = config_entry
 
     async def async_step_init(self, user_input=None):
         if user_input is not None:
             return self.async_create_entry(title="", data=user_input)
 
-        schema = vol.Schema({
-            vol.Required(CONF_PRODUCTION_SENSOR, default=self.config_entry.data.get(CONF_PRODUCTION_SENSOR, "")): str,
-            vol.Required(CONF_CONSOMMATION_SENSOR, default=self.config_entry.data.get(CONF_CONSOMMATION_SENSOR, "")): str,
-        })
-
-        return self.async_show_form(step_id="init", data_schema=schema)
+        return self.async_show_form(
+            step_id="init",
+            data_schema=vol.Schema({
+                vol.Required(CONF_PRODUCTION_SENSOR, default=self.config_entry.data.get(CONF_PRODUCTION_SENSOR, "")): selector({
+                    "entity": {
+                        "domain": "sensor",
+                        "device_class": "energy",
+                    }
+                }),
+                vol.Required(CONF_CONSOMMATION_SENSOR, default=self.config_entry.data.get(CONF_CONSOMMATION_SENSOR, "")): selector({
+                    "entity": {
+                        "domain": "sensor",
+                        "device_class": "energy",
+                    }
+                }),
+            })
+        )
