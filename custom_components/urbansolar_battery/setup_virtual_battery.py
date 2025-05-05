e4119a423b11223892a1f5e962c2049011a0a1be
chourmovs <chourmovs@gmail.com>
2025-05-05 09:16:24 +0200

    fix unit 2

--- custom_components/urbansolar_battery/setup_virtual_battery.py
+++ custom_components/urbansolar_battery/setup_virtual_battery.py
@@ -75,10 +75,10 @@ async def setup_virtual_battery(hass: HomeAssistant, entry: ConfigEntry) -> None
             "sensors": {
                 "urban_puissance_import_enedis": {
                     "friendly_name": "Urban Puissance Import Enedis",
-                    "unit_of_measurement": "kW",
+                    "unit_of_measurement": "W",
                     "value_template": (
-                        "{% set puissance_conso = states('" + str(cons_instant) + "') | float(0) * 1000 %}\n"
-                        "{% set puissance_prod = states('" + str(prod_instant) + "') | float(0) * 1000 %}\n"
+                        "{% set puissance_conso = states('" + str(cons_instant) + "') | float(0) %}\n"
+                        "{% set puissance_prod = states('" + str(prod_instant) + "') | float(0)  %}\n"
                         "{% set batterie_stock = states('input_number.urban_batterie_virtuelle_stock') | float(0) %}\n"
                         "{% if batterie_stock > 0 %} 0\n"
                         "{% elif (puissance_conso - puissance_prod) > 0 %}\n"
